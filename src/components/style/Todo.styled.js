310adc8ae942a11686eedb632a611c742ac29669
agdgom18 <agdgomelashvili18@gmail.com>
2023-07-06 10:57:33 +0400

    add styled component for TodoList

---
+++ src/components/style/Todo.styled.js
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+export const StyledTodo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #8758ff;
+  color: #f8f6f6;
+  padding: 0.75rem 1rem;
+  border-radius: 5px;
+  margin-bottom: 1rem;
+  cursor: pointer;
+`;
